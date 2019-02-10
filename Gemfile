4e0811d012509d8b10120226cda80a2703970cda
yysaki <yysaki@mail.yysaki.com>
2019-02-10 22:10:19 +0900

    Run todo server connected to AWS RDS with DB_URL

--- Gemfile
+++ Gemfile
@@ -6,9 +6,9 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 gem 'activerecord'
+gem 'mysql2'
 gem 'rack_csrf'
 gem 'rubocop'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'solargraph'
-gem 'sqlite3'
--- app.rb
+++ app.rb
@@ -2,10 +2,51 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+require 'rack/csrf'
+
+use Rack::Session::Cookie, secret: 'thisissomethingsecret'
+use Rack::Csrf, raise: true
+
+helpers do
+  def csrf_tag
+    Rack::Csrf.csrf_tag(env)
+  end
+
+  def csrf_token
+    Rack::Csrf.csrf_token(env)
+  end
+
+  def h(str)
+    Rack::Utils.escape_html(str)
+  end
+end
+
+ActiveRecord::Base.establish_connection(ENV['DB_URL'])
+# ActiveRecord::Base.establish_connection(
+#   adapter: 'mysql2',
+#   host: 'workout-recording-db.ccaiowlzfjrk.ap-northeast-1.rds.amazonaws.com',
+#   username: 'sasaki',
+#   password: 'onTiBRyPOlEcOunOsERfusHaBIoulT',
+#   database: 'workout_recording'
+# )
+
+# test
+class Comment < ActiveRecord::Base
+  validates :body, presence: true
+end
 
 get '/' do
-  headers \
-    'Access-Control-Allow-Origin' => '*'
+  @title = 'My BBS'
+  @comments = Comment.all
+  erb :index
+end
 
-  body 'hello sinatra-workout-recording'
+post '/create' do
+  Comment.create(body: params[:body])
+  redirect to('/')
+end
+
+post '/destroy' do
+  Comment.find(params[:id]).destroy
 end
