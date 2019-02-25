df1e4b1f025eb81be580fce8d4a708cdf6488400
yysaki <yysaki@mail.yysaki.com>
2019-02-25 21:30:00 +0900

    Validates User.name uniqueness

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :password, presence: true
 end
